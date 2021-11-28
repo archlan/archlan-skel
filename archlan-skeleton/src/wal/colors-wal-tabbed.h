a97f90ec3ca56529484ea60d9c0003db6b212410
Lanhild <joliveau.loan@gmail.com>
2021-11-28 16:33:03 -0500

    update contents

--- archlan-skeleton/src/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#02050b";
-static const char* selfgcolor   = "#bfc0c2";
-static const char* normbgcolor  = "#844833";
-static const char* normfgcolor  = "#bfc0c2";
-static const char* urgbgcolor   = "#3f3f53";
-static const char* urgfgcolor   = "#bfc0c2";
