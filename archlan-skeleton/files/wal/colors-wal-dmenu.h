9f9d838a16dfad487bb85f47f7dcded2ccc9073a
Lanhild <archlan@protonmail.com>
2022-01-10 13:51:59 -0500

    create archlan-zsh

--- archlan-skeleton/files/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bfc0c2", "#02050b" },
-	[SchemeSel] = { "#bfc0c2", "#3f3f53" },
-	[SchemeOut] = { "#bfc0c2", "#566D92" },
-};
